0c8c72db2610be5bcf814bfd7ecad541732f19a4
Fedor M <37484405+FedorMi@users.noreply.github.com>
2023-10-31 16:59:09 +0100

    first commit
    gui is the main function, which you need to run to visualize the algorithms

---
+++ gui.py
@@ -0,0 +1,393 @@
+from PyQt5 import QtWidgets, QtCore, QtGui, uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGridLayout, QPushButton, QWidget, QHBoxLayout, QVBoxLayout, QComboBox,QScrollArea
+import sys
+import numpy as np
+from random import randint, choice
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import sys
+import time
+from search_and_sort_algorithms import Sort_Worker, Search_Worker
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Python ")
+        self.setGeometry(100, 100, 1400, 920)
+        self.choice = 0
+        self.small, self.large, self.amount = 0, 1000, 50
+        self.layout = QVBoxLayout()
+        choose_mode = QComboBox()
+        choose_mode.setStyleSheet("background-color: rgb(20, 20, 35); color: rgb(0, 255, 0); border:1px solid rgb(0, 255, 0); font: 18px")
+        self.current_wrapper = QWidget()
+        self.sort_layout = QVBoxLayout()
+        self.current_wrapper.setLayout(self.sort_layout)
+        self.setStyleSheet("background-color: rgb(20, 20, 35);")
+        modes = ["Sorting", "Searching"]
+        choose_mode.addItems(modes)
+        choose_mode.currentIndexChanged.connect(self.sort_or_search)
+        self.setLayout(self.layout)
+        self.layout.addWidget(choose_mode)
+        self.setup_sorting_layout(self.sort_layout)
+        self.layout.addWidget(self.current_wrapper)
+        self.show()
+
+    def sort_or_search(self, choice):
+        self.choice = choice
+        if choice:
+            self.layout.removeWidget(self.current_wrapper)
+            self.current_wrapper.close()
+            self.current_wrapper = QWidget()
+            self.search_layout = QVBoxLayout()
+            self.current_wrapper.setLayout(self.search_layout)
+            self.setup_searching_layout(self.search_layout)
+            self.layout.addWidget(self.current_wrapper)
+            self.search_num = 0
+        else:
+            self.layout.removeWidget(self.current_wrapper)
+            self.current_wrapper.close()
+            self.current_wrapper = QWidget()
+            self.sort_layout = QVBoxLayout()
+            self.current_wrapper.setLayout(self.sort_layout)
+            self.setup_sorting_layout(self.sort_layout)
+            self.layout.addWidget(self.current_wrapper)
+            self.sort_num = 0
+
+    # searching functions
+    def setup_searching_layout(self, search_layout):
+        self.pixw = 1400
+        self.pixh = 800
+        self.label = QtWidgets.QLabel()
+        button_new = QPushButton("New Random Array", self)
+        button_new.setStyleSheet("QPushButton {background-color: rgb(20, 20, 35); color: rgb(0, 255, 0); border:1px solid rgb(0, 255, 0); font: 18px} QPushButton::hover {background-color: rgb(20, 100, 35);}")
+        button_new.clicked.connect(self.setup_random_array_search)
+        select_searching_algo = QComboBox()
+        select_searching_algo.setStyleSheet("background-color: rgb(20, 20, 35); color: rgb(0, 255, 0); border:1px solid rgb(0, 255, 0); font: 18px")
+        searching_algos = ["Linear Search", "Binary Search"]
+        select_searching_algo.addItems(searching_algos)
+        select_searching_algo.currentIndexChanged.connect(self.search_choice)
+        self.search_num = 0
+        button_searching = QPushButton("Run Searching Algorithm", self)
+        button_searching.setStyleSheet("QPushButton {background-color: rgb(20, 20, 35); color: rgb(0, 255, 0); border:1px solid rgb(0, 255, 0); font: 18px} QPushButton::hover {background-color: rgb(20, 100, 35);}")
+        button_searching.clicked.connect(self.run_searching)
+        button_sort = QPushButton("Sort the list", self)
+        button_sort.setStyleSheet("QPushButton {background-color: rgb(20, 20, 35); color: rgb(0, 255, 0); border:1px solid rgb(0, 255, 0); font: 18px} QPushButton::hover {background-color: rgb(20, 100, 35);}")
+        button_sort.clicked.connect(self.run_sort)
+        select_size = QComboBox()
+        select_size.setStyleSheet("background-color: rgb(20, 20, 35); color: rgb(0, 255, 0); border:1px solid rgb(0, 255, 0); font: 18px")
+        choose_a_size = ["Choose Size of List", "8", "16", "50", "100", "200", "300", "400", "500"]
+        select_size.addItems(choose_a_size)
+        select_size.currentIndexChanged.connect(self.choose_size_search)
+        top_row = QHBoxLayout()
+        top_row.addWidget(button_new)
+        top_row.addWidget(select_searching_algo)
+        top_row.addWidget(button_searching)
+        top_row.addWidget(button_sort)
+        top_row.addWidget(select_size)
+        search_layout.addLayout(top_row)
+        self.bottom_row = QHBoxLayout()
+        how_many = self.amount if self.amount <= 16 else 16
+        for i in range(how_many):
+            button = QPushButton(str(i), self)
+            button.setStyleSheet("QPushButton {background-color: rgb(20, 20, 35); color: rgb(0, 255, 0); border:1px solid rgb(0, 255, 0); font: 18px} QPushButton::hover {background-color: rgb(20, 100, 35);}")
+            button.setObjectName(str(i))
+            button.clicked.connect(self.show_number)
+            self.bottom_row.addWidget(button)
+        search_layout.addWidget(self.label)
+        search_layout.addLayout(self.bottom_row)
+        self.label.setStyleSheet("border:1px solid rgb(0, 255, 0);")
+        canvas = QtGui.QPixmap(self.pixw, self.pixh)
+        self.label.setPixmap(canvas)
+        self.threadpool = QThreadPool()
+        self.setup_random_array_search()
+    
+    def search_choice(self, num):
+        self.search_num = num
+
+    def choose_size_search(self, num):
+        amounts = [50, 8, 16, 50, 100, 200, 300, 400, 500]
+        self.amount = amounts[num]
+        self.setup_random_array_search()
+    
+    def run_searching(self):
+        worker = Search_Worker(self.arr, self.guess_number, self.search_num)
+        worker.signals.guess.connect(self.draw_guess_line)
+        self.threadpool.start(worker)
+    def run_sort(self):
+        self.arr.sort()
+        self.draw_array_with_outline_search()
+
+    def show_number(self):
+        i = int(self.sender().objectName())
+        self.draw_guess_line(i)
+    def draw_guess_line(self, i):
+        painter = QtGui.QPainter(self.label.pixmap())
+        myx = int(self.aw*i/self.lengtho)+int(self.pen_width/2)
+        myy = int(self.ah*(self.arr[i]/self.highest))
+        self.pen.setColor(QtGui.QColor('black'))
+        self.pen.setWidth(int(self.pen_width))
+        painter.setPen(self.pen)
+        painter.drawLine(myx, self.ah, myx, self.ah-myy)
+        if self.guess_number == self.arr[i]:
+            self.pen.setColor(QtGui.QColor('green'))
+            self.pen.setWidth(int(self.pen_width-2))
+            painter.setPen(self.pen)
+            painter.drawLine(myx, self.ah, myx, self.ah-myy)
+            print("correct guess")
+        else:
+            self.pen.setColor(QtGui.QColor('red'))
+            self.pen.setWidth(int(self.pen_width-2))
+            painter.setPen(self.pen)
+            painter.drawLine(myx, self.ah, myx, self.ah-myy)
+            if self.guess_number < self.arr[i]:
+                print("smaller")
+            else:
+                print("bigger")
+
+        self.update()
+    
+    def setup_random_array_search(self):
+        self.arr = np.random.randint(self.small, self.large, self.amount)
+        #self.arr.sort()
+        self.guess_number = choice(self.arr)
+        self.draw_array_with_outline_search()
+
+    def draw_array_with_outline_search(self):
+        lengtho, aw, ah, highest, painter, pen, pen_width = self.setup_draw()
+        for i, k in enumerate(self.arr):
+            myx = int(aw*i/lengtho)+int(pen_width/2)
+            myy = int(ah*(k/highest))
+            pen.setColor(QtGui.QColor('black'))
+            pen.setWidth(int(pen_width))
+            painter.setPen(pen)
+            painter.drawLine(myx, ah, myx, ah-myy)
+            pen.setColor(QtGui.QColor('white'))
+            pen.setWidth(int(pen_width-2))
+            painter.setPen(pen)
+            painter.drawLine(myx, ah, myx, ah-myy)
+        self.lengtho, self.aw, self.ah, self.highest, self.pen, self.pen_width = lengtho, aw, ah, highest, pen, pen_width 
+        self.update()
+    
+    # sorting functions
+    def setup_sorting_layout(self, sort_layout):
+        self.pixw = 1400
+        self.pixh = 800
+        self.label = QtWidgets.QLabel()
+        button = QPushButton("New Random Array", self)
+        button.setStyleSheet("QPushButton {background-color: rgb(20, 20, 35); color: rgb(0, 255, 0); border:1px solid rgb(0, 255, 0); font: 18px} QPushButton::hover {background-color: rgb(20, 100, 35);}")
+        button.clicked.connect(self.setup_random_array)
+        select_sorting_algo = QComboBox()
+        select_sorting_algo.setStyleSheet("background-color: rgb(20, 20, 35); color: rgb(0, 255, 0); border:1px solid rgb(0, 255, 0); font: 18px")
+        sorting_algos = ["Bubble Sort", "Selection Sort", "Quick Sort", "Merge Sort", "Heap Sort"]
+        select_sorting_algo.addItems(sorting_algos)
+        select_sorting_algo.currentIndexChanged.connect(self.sort_choice)
+        self.sort_num = 0
+        button_sorting = QPushButton("Run Sorting Algorithm", self)
+        button_sorting.setStyleSheet("QPushButton {background-color: rgb(20, 20, 35); color: rgb(0, 255, 0); border:1px solid rgb(0, 255, 0); font: 18px} QPushButton::hover {background-color: rgb(20, 100, 35);}")
+        button_sorting.clicked.connect(self.run_sorting)
+        select_size = QComboBox()
+        select_size.setStyleSheet("background-color: rgb(20, 20, 35); color: rgb(0, 255, 0); border:1px solid rgb(0, 255, 0); font: 18px")
+        choose_a_size = ["Choose Size of List", "8", "16", "50", "100", "200", "300", "400", "500"]
+        select_size.addItems(choose_a_size)
+        select_size.currentIndexChanged.connect(self.choose_size_sort)
+        top_row = QHBoxLayout()
+        top_row.addWidget(button)
+        top_row.addWidget(select_sorting_algo)
+        top_row.addWidget(button_sorting)
+        top_row.addWidget(select_size)
+        self.bottom_row = QHBoxLayout()
+        self.switch_places = []
+        how_many = self.amount if self.amount <= 16 else 16
+        for i in range(how_many):
+            button = QPushButton(str(i), self)
+            button.setStyleSheet("QPushButton {background-color: rgb(20, 20, 35); color: rgb(0, 255, 0); border:1px solid rgb(0, 255, 0); font: 18px} QPushButton::hover {background-color: rgb(20, 100, 35);}")
+            button.setObjectName(str(i))
+            button.clicked.connect(self.show_number_sort)
+            self.bottom_row.addWidget(button)
+        sort_layout.addLayout(top_row)
+        sort_layout.addWidget(self.label)
+        sort_layout.addLayout(self.bottom_row)
+        self.label.setStyleSheet("border:1px solid rgb(0, 255, 0);")
+        canvas = QtGui.QPixmap(self.pixw, self.pixh)
+        self.label.setPixmap(canvas)
+        self.threadpool = QThreadPool()
+        self.setup_random_array()
+        #self.setup_sorting_steps()
+    
+    def show_number_sort(self):
+        i = int(self.sender().objectName())
+        self.draw_sort_line(i)
+        self.switch_places.append(i)
+        if len(self.switch_places) > 1:
+            self.arr[self.switch_places[0]], self.arr[self.switch_places[1]] = self.arr[self.switch_places[1]], self.arr[self.switch_places[0]]
+            self.draw_aray_with_outline()
+            self.switch_places = []
+
+    def draw_sort_line(self, i):
+        painter = QtGui.QPainter(self.label.pixmap())
+        myx = int(self.aw*i/self.lengtho)+int(self.pen_width/2)
+        myy = int(self.ah*(self.arr[i]/self.highest))
+        self.pen.setColor(QtGui.QColor('black'))
+        self.pen.setWidth(int(self.pen_width))
+        painter.setPen(self.pen)
+        painter.drawLine(myx, self.ah, myx, self.ah-myy)
+        self.pen.setColor(QtGui.QColor('red'))
+        self.pen.setWidth(int(self.pen_width-2))
+        painter.setPen(self.pen)
+        painter.drawLine(myx, self.ah, myx, self.ah-myy)
+        self.update()
+    
+    def choose_size_sort(self, num):
+        amounts = [50, 8, 16, 50, 100, 200, 300, 400, 500]
+        self.amount = amounts[num]
+        self.setup_random_array()
+    
+    def choose_size_sort_workinprogress(self, num):
+        amounts = [50, 8, 16, 50, 100, 200, 300, 400, 500]
+        for i in range(self.bottom_row.count()):
+            myWidget = self.bottom_row.itemAt(i)
+            self.bottom_row.removeItem(myWidget)
+        self.sort_layout.removeItem(self.bottom_row)
+        self.amount = amounts[num]
+        self.bottom_row = QHBoxLayout()
+        self.setup_random_array()
+        self.switch_places = []
+        how_many = self.amount if self.amount <= 16 else 16
+        for i in range(how_many):
+            button = QPushButton(str(i), self)
+            button.setStyleSheet("QPushButton {background-color: rgb(20, 20, 35); color: rgb(0, 255, 0); border:1px solid rgb(0, 255, 0); font: 18px} QPushButton::hover {background-color: rgb(20, 100, 35);}")
+            button.setObjectName(str(i))
+            button.clicked.connect(self.show_number_sort)
+            self.bottom_row.addWidget(button)
+        self.sort_layout.addLayout(self.bottom_row)
+        self.update()
+    
+    def setup_random_array(self):
+        self.arr = np.random.randint(self.small, self.large, self.amount)
+        self.draw_aray_with_outline()
+    
+    def sort_choice(self, num):
+        self.sort_num = num
+    
+    def run_sorting(self):
+        worker = Sort_Worker(self.arr, self.sort_num)
+        worker.signals.progress.connect(self.draw_aray_show_with_outline)
+        worker.signals.finished.connect(self.draw_aray_with_outline)
+        self.threadpool.start(worker)
+
+    def draw_aray(self):
+        lengtho, aw, ah, highest, painter, pen, pen_width = self.setup_draw()
+        for i, k in enumerate(self.arr):
+            myx = int(aw*i/lengtho)+int(pen_width/2)
+            myy = int(ah*(k/highest))
+            painter.drawLine(myx, ah, myx, ah-myy)
+        self.update()
+
+    def draw_aray_show(self, a, b):
+        lengtho, aw, ah, highest, painter, pen, pen_width = self.setup_draw()
+        for i, k in enumerate(self.arr):
+            myx = int(aw*i/lengtho)+int(pen_width/2)
+            myy = int(ah*(k/highest))
+            if i == a or i == b:
+                pen.setColor(QtGui.QColor('red'))
+                painter.setPen(pen)
+                painter.drawLine(myx, ah, myx, ah-myy)
+                pen.setColor(QtGui.QColor('green'))
+                painter.setPen(pen)
+            else:
+                painter.drawLine(myx, ah, myx, ah-myy)
+        self.lengtho, self.aw, self.ah, self.highest, self.pen, self.pen_width = lengtho, aw, ah, highest, pen, pen_width 
+        self.update()
+
+    def draw_aray_with_outline(self):
+        lengtho, aw, ah, highest, painter, pen, pen_width = self.setup_draw()
+        for i, k in enumerate(self.arr):
+            myx = int(aw*i/lengtho)+int(pen_width/2)
+            myy = int(ah*(k/highest))
+            pen.setColor(QtGui.QColor('black'))
+            pen.setWidth(int(pen_width))
+            painter.setPen(pen)
+            painter.drawLine(myx, ah, myx, ah-myy)
+            pen.setColor(QtGui.QColor('green'))
+            pen.setWidth(int(pen_width-2))
+            painter.setPen(pen)
+            painter.drawLine(myx, ah, myx, ah-myy)
+        self.lengtho, self.aw, self.ah, self.highest, self.pen, self.pen_width = lengtho, aw, ah, highest, pen, pen_width 
+        self.update()
+
+    def draw_aray_show_with_outline(self, a, b):
+        lengtho, aw, ah, highest, painter, pen, pen_width = self.setup_draw()
+        for i, k in enumerate(self.arr):
+            myx = int(aw*i/lengtho)+int(pen_width/2)
+            myy = int(ah*(k/highest))
+            pen.setColor(QtGui.QColor('black'))
+            pen.setWidth(int(pen_width))
+            painter.setPen(pen)
+            painter.drawLine(myx, ah, myx, ah-myy)
+            pen.setWidth(int(pen_width-2))
+            if i == a or i == b:
+                pen.setColor(QtGui.QColor('red'))
+                painter.setPen(pen)
+                painter.drawLine(myx, ah, myx, ah-myy)
+                pen.setColor(QtGui.QColor('green'))
+                painter.setPen(pen)
+            else:
+                pen.setColor(QtGui.QColor('green'))
+                painter.setPen(pen)
+                painter.drawLine(myx, ah, myx, ah-myy)
+        self.lengtho, self.aw, self.ah, self.highest, self.pen, self.pen_width = lengtho, aw, ah, highest, pen, pen_width 
+        self.update()
+
+    def draw_line(self, pen, pen_color, pen_width, startx, starty, endx, endy):
+        pass
+
+    def setup_draw(self):
+        self.label.clear()
+        canvas = QtGui.QPixmap(self.pixw, self.pixh)
+        self.label.setPixmap(canvas)
+        lengtho = len(self.arr)
+        aw = self.pixw
+        ah = self.pixh
+        highest = max(self.arr)
+        painter = QtGui.QPainter(self.label.pixmap())
+        pen = QtGui.QPen()
+        pen.setColor(QtGui.QColor('green'))
+        pen_width = aw/lengtho
+        pen.setWidth(int(pen_width))
+        painter.setPen(pen)
+        return lengtho, aw, ah, highest, painter, pen, pen_width
+
+    def setup_sorting_steps(self):
+        self.i = 0
+        self.j = 0
+        bottom_row = QHBoxLayout()
+        select_sorting_algo = QComboBox()
+        select_sorting_algo.setStyleSheet("background-color: rgb(20, 20, 35); color: rgb(0, 255, 0); border:1px solid rgb(0, 255, 0); font: 18px")
+        sorting_algos = ["Bubble Sort", "Selection Sort", "Quick Sort", "Merge Sort", "Heap Sort"]
+        select_sorting_algo.addItems(sorting_algos)
+        button1 = QPushButton("New Random Array", self)
+        button1.setStyleSheet("QPushButton {background-color: rgb(20, 20, 35); color: rgb(0, 255, 0); border:1px solid rgb(0, 255, 0); font: 18px} QPushButton::hover {background-color: rgb(20, 100, 35);}")
+        button2 = QPushButton("STEP", self)
+        button2.setStyleSheet("QPushButton {background-color: rgb(20, 20, 35); color: rgb(0, 255, 0); border:1px solid rgb(0, 255, 0); font: 18px} QPushButton::hover {background-color: rgb(20, 100, 35);}")
+        bottom_row.addWidget(select_sorting_algo)
+        bottom_row.addWidget(button1)
+        bottom_row.addWidget(button2)
+        self.sort_layout.addLayout(bottom_row)
+
+
+def main():
+    print("sorting")
+    sort_this = []
+    n = 20
+    for i in range(100):
+        sort_this.append(randint(1, n))
+    print(sort_this)
+    print(sort_this)
+
+
+if __name__ == "__main__":
+    App = QApplication(sys.argv)
+    window = Window()
+    sys.exit(App.exec())
---
+++ search_and_sort_algorithms.py
@@ -0,0 +1,185 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+from random import randint, choice
+import time
+
+
+class Sort_Worker(QRunnable):
+    def __init__(self, arr = [3, 2, 1], choice = 0):
+        super(Sort_Worker, self).__init__()
+        self.arr = arr
+        self.signals = WorkerSignals()
+        sorting_algos = [self.bubble_sort, self.selection_sort, self.quick_sort, self.merge_sort, self.heap_sort]
+        self.fn = sorting_algos[choice]
+
+    def run(self):
+        self.fn()
+
+    def bubble_sort(self):
+        arr = self.arr
+        n = len(arr)
+        for i in range(n):
+            for j in range(n-1):
+                if self.arr[j]>arr[j+1]:
+                    temp = arr[j]
+                    arr[j] = arr[j+1]
+                    arr[j+1] = temp
+                    self.signals.progress.emit(j+1, j)
+                    time.sleep(0.01)
+        self.signals.finished.emit()
+    def selection_sort(self):
+        arr = self.arr
+        n = len(arr)
+        for i in range(n):
+            j = i
+            while j > 0 and arr[j]<arr[j-1]:
+                temp = arr[j-1]
+                arr[j-1] = arr[j]
+                arr[j] = temp
+                self.signals.progress.emit(j-1, j)
+                time.sleep(0.01)
+                j -= 1
+        self.signals.finished.emit()
+    def quick_sort(self, begin=0, end=None):
+        arr = self.arr
+        if end is None:
+            end = len(arr) - 1
+        def _quick_sort(begin, end):
+            if begin >= end:
+                return
+            pivot = self.quick_helper(begin, end)
+            _quick_sort(begin, pivot-1)
+            _quick_sort(pivot+1, end)
+        return _quick_sort(begin, end)
+    def quick_helper(self, begin, end):
+        arr = self.arr
+        pivot = begin
+        for i in range(begin+1, end+1):
+            if arr[i] <= arr[begin]:
+                pivot += 1
+                arr[i], arr[pivot] = arr[pivot], arr[i]
+                self.signals.progress.emit(i, pivot)
+                time.sleep(0.01)
+        arr[pivot], arr[begin] = arr[begin], arr[pivot]
+        self.signals.progress.emit(pivot, begin)
+        time.sleep(0.01)
+        return pivot
+    def merge_sort(self):
+        arr = self.arr
+        self.merge_sort_helper(arr, 0, len(arr)-1)
+    def merge_sort_helper(self, arr, l, r):
+        if (l < r):
+            # Same as (l + r) / 2, but avoids overflow
+            # for large l and r
+            m = l + (r - l) // 2
+            # Sort first and second halves
+            self.merge_sort_helper(arr, l, m)
+            self.merge_sort_helper(arr, m + 1, r)
+            self.merge(arr, l, m, r)
+    def merge(self, arr, start, mid, end):
+        start2 = mid + 1
+        # If the direct merge is already sorted
+        if (arr[mid] <= arr[start2]):
+            return
+        # Two pointers to maintain start
+        # of both arrays to merge
+        while (start <= mid and start2 <= end):
+            # If element 1 is in right place
+            if (arr[start] <= arr[start2]):
+                start += 1
+            else:
+                value = arr[start2]
+                index = start2
+                # Shift all the elements between element 1
+                # element 2, right by 1.
+                while (index != start):
+                    arr[index] = arr[index - 1]
+                    index -= 1
+                    self.signals.progress.emit(index, index - 1)
+                    time.sleep(0.01)
+                arr[start] = value
+                self.signals.progress.emit(start, start2)
+                time.sleep(0.01)
+                # Update all the pointers
+                start += 1
+                mid += 1
+                start2 += 1
+    def heap_sort(self):
+        arr = self.arr
+        n = len(arr)
+        # Build max heap
+        for i in range(n//2, -1, -1):
+            self.heapify(arr, n, i)
+        for i in range(n-1, 0, -1):
+            arr[i], arr[0] = arr[0], arr[i] # Swap
+            self.signals.progress.emit(i, 0)
+            time.sleep(0.01)
+            self.heapify(arr, i, 0) # Heapify root element
+    def heapify(self, arr, n, i):
+        # Find largest among root and children
+        largest = i
+        l = 2 * i + 1
+        r = 2 * i + 2
+        if l < n and arr[i] < arr[l]:
+            largest = l
+        if r < n and arr[largest] < arr[r]:
+            largest = r
+        # If root is not largest, swap with largest and continue heapifying
+        if largest != i:
+            arr[i], arr[largest] = arr[largest], arr[i]
+            self.signals.progress.emit(largest, i)
+            time.sleep(0.01)
+            self.heapify(arr, n, largest)
+
+class Search_Worker(QRunnable):
+    def __init__(self, arr = [3, 2, 1], guess = 1, choice = 0):
+        super(Search_Worker, self).__init__()
+        self.guess = guess
+        self.arr = arr
+        self.signals = WorkerSignals()
+        searching_algos = [self.linear_search, self.binary_search]
+        self.fn = searching_algos[choice]
+
+    def run(self):
+        self.fn()
+
+    def linear_search(self):
+        arr = self.arr
+        n = len(arr)
+        for i in range(n):
+            self.signals.guess.emit(i)
+            time.sleep(0.04)
+            if self.guess == self.arr[i]:
+                break
+    def binary_search(self):
+        arr = self.arr
+        self.binary_search_helper(arr, 0, len(arr)-1, self.guess)
+    def binary_search_helper(self, arr, low, high, x):
+        # Check base case
+        if high >= low:
+            mid = (high + low) // 2
+            # If element is present at the middle itself
+            self.signals.guess.emit(mid)
+            time.sleep(0.5)
+            if arr[mid] == x:
+                print("found")
+            # If element is smaller than mid, then it can only
+            # be present in left subarray
+            elif arr[mid] > x:
+                self.binary_search_helper(arr, low, mid - 1, x)
+            # Else the element can only be present in right subarray
+            else:
+                self.binary_search_helper(arr, mid + 1, high, x)
+        else:
+            # Element is not present in the array
+            print("not in array")
+
+class WorkerSignals(QObject):
+    finished = pyqtSignal()
+    error = pyqtSignal(tuple)
+    result = pyqtSignal(object)
+    list = pyqtSignal(list)
+    progress = pyqtSignal(int, int)
+    guess = pyqtSignal(int)
